c57446298b8d6b4802ee1fff06d218f0f1dba706
Savina Shen (Manpower Services Taiwan Co Ltd) <a-savinashen@microsoft.com>
2024-07-17 18:20:37 +0800

    show red div

--- src/stories/DefaultMap.tsx
+++ src/stories/DefaultMap.tsx
@@ -4,15 +4,15 @@ import { AzureMapsProvider } from '../contexts/AzureMapContext'
 import { IAzureMapOptions } from '../types'
 import { AuthenticationType } from '../types'
 
-const option: IAzureMapOptions = {
-  authOptions: {
-    authType: AuthenticationType.subscriptionKey,
-    subscriptionKey: process.env.react_app_subscription_key // Your subscription key
-  }
-}
+// const option: IAzureMapOptions = {
+//   authOptions: {
+//     authType: AuthenticationType.subscriptionKey,
+//     subscriptionKey: process.env.react_app_subscription_key // Your subscription key
+//   }
+// }
 
 const DefaultMap = () => {
-  return <div style={{ width: '200px', height: '100px', backgroundColor: 'red' }}></div>
+  return <div style={{ width: '200px', height: '100px', backgroundColor: 'red' }}>Hello</div>
 }
 
 export default DefaultMap
